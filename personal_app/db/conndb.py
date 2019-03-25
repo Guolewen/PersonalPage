2f3032cb796287c5b87aa30f3416fed09ac422cd
Lewen, Guo <l_guo@hyas.co.jp>
2019-03-25 17:29:15 +0900

    190325hyas

--- personal_app/db/conndb.py
+++ personal_app/db/conndb.py
@@ -7,13 +7,8 @@ db_name = os.environ.get('CLOUD_SQL_DATABASE_NAME')
 
 def connect2db():
     if os.environ.get('GAE_ENV') == 'standard':
-
         cloud_sql_connection_name = os.environ.get("CLOUD_SQL_CONNECTION_NAME")
-        db = sqlalchemy.create_engine(f'mysql+pymysql://{db_user}:{db_password}@/{db_name}?unix_socket=/cloudsql/'
-                                      f'{cloud_sql_connection_name}/personal',
-                                      pool_size=5,
-                                      pool_timeout=30,
-                                      pool_recycle=1800)
+        db = sqlalchemy.create_engine(f'mysql+pymysql://{db_user}:{db_password}@/{db_name}?unix_socket=/cloudsql/{cloud_sql_connection_name}')
     else:
         db = sqlalchemy.create_engine(f'mysql+pymysql://root:Guolewen1234@35.241.95.147/personal')
     return db
